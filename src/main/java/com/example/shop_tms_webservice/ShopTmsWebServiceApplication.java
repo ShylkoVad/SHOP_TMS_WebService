042cc442590af619c1bef7519d4c08e6749fce23
Vadzim <shylkovad@gmail.com>
2023-09-23 13:22:15 +0300

    WebService

---
+++ src/main/java/com/example/shop_tms_webservice/ShopTmsWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.shop_tms_webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopTmsWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopTmsWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shop_tms_webservice/ShopTmsWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shop_tms_webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopTmsWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
